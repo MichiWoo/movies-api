195ee96dd9f62e40f37affb7bbd9cc187028e6f2
MichiWoo <michiwoo.web@gmail.com>
2020-06-21 21:10:19 -0500

    Middleware - Manejo de Errores

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const config = {
-  dev: process.env.NODE_ENV !== 'produccion',
+  dev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || 3000,
   CORS: process.env.CORS,
   dbUser: process.env.DB_USER,
--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 
 const { config } = require('./config/index')
-
 const { moviesApi } = require('./routes/movies.js')
+
+//Middlewares
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers')
 
 // Body parse (Para intereprepeat los datos del json)
 app.use(express.json())
 
 moviesApi(app)
+
+app.use(logErrors)
+app.use(errorHandler)
 
 app.listen(config.port, ()=> {
   console.log(`Listening http://localhost:${config.port}`)
